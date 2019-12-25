7e75f05a70c107338bb1a3da2652954ed23a95a3
ustcfjh <282844156@qq.com>
2019-12-25 15:20:57 +0800

    syn

--- src/main/java/com/jiahui/personalsite/test/TestSite.java
+++ src/main/java/com/jiahui/personalsite/test/TestSite.java
@@ -2,6 +2,8 @@ package com.jiahui.personalsite.test;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class TestSite {
     public static void main(String args[]){
@@ -11,8 +13,16 @@ public class TestSite {
             e.printStackTrace();
         }
 
-        try(Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/")){
-
+        try(Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/blog?useUnicode=true&characterEncoding=utf8&serverTimezone=GMT%2B8","root","Sx6215106..");
+            Statement s = c.createStatement();
+        ){
+            for(int i=1;i<=5;i++){
+                String sqlFormat = "insert into category values(null,'测试分类%d')";
+                String sql = String.format(sqlFormat,i);
+                s.execute(sql);
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
         }
     }
 }
--- src/test/java/com/jiahui/personalsite/PersonalsiteApplicationTests.java
+++ src/test/java/com/jiahui/personalsite/PersonalsiteApplicationTests.java
@@ -1,13 +1,5 @@
 package com.jiahui.personalsite;
 
-import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class PersonalsiteApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
